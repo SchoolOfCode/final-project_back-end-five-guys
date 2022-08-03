fcf5348ad734845287fb22a7aa1251afcc0a23ed
brycen <brycensoc@gmail.com>
2022-08-03 15:58:13 +0100

    diary done

--- models/diary.js
+++ models/diary.js
@@ -2,6 +2,18 @@ import pool from '../db/index.js';
 
 export async function getDiary() {
   const res = await pool.query('select * from diary');
+  return res.rows;
 }
 
-export async function getPatients() {}
+export async function getDiaryById(id) {
+  const res = await pool.query('select * from diary where patient_id=$1', [id]);
+  return res.rows;
+}
+
+export async function newDiaryEntry(id, entry) {
+  const res = await pool.query(
+    'insert into diary (patient_id,date,mood,details) values ($1,$2,$3,$4) returning *;',
+    [id, entry.date, entry.mood, entry.details]
+  );
+  return res.rows;
+}
