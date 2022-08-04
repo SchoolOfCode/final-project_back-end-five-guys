cf30013e6184360b188834048ca5f83eded872a7
BPGreenwood <53783989+B-P-Greenwood@users.noreply.github.com>
2022-08-04 10:41:52 +0100

    Created dummy patient data for DB

--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,19 @@
-import express from 'express';
-import { getDiary, getDiaryById, newDiaryEntry } from '../models/diary.js';
+import express from "express";
+import { getDiary, getDiaryById, newDiaryEntry } from "../models/diary.js";
 import {
   getAllergies,
   getAllergiesById,
   makeAllergy,
-} from '../models/allergy.js';
-import { getSignUps, newSignUp, linkSignUp } from '../models/signUp.js';
+} from "../models/allergy.js";
+import { getSignUps, newSignUp, linkSignUp } from "../models/signUp.js";
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
-  res.send('Home page not in use');
+router.get("/", function (req, res, next) {
+  res.send("Home page not in use");
 });
 
-router.get('/diary', async function (req, res, next) {
+router.get("/diary", async function (req, res, next) {
   const response = await getDiary();
   res.json({ success: true, data: response });
 });
