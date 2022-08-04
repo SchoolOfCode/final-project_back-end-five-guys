23a92500e70cb1e57e9ea1c01edbe73c6ae7a2b5
BPGreenwood <53783989+B-P-Greenwood@users.noreply.github.com>
2022-08-04 17:13:43 +0100

    Added route to get patient allergies

--- routes/users.js
+++ routes/users.js
@@ -1,25 +1,24 @@
-import express from 'express';
-import { getDiary, getDiaryById, newDiaryEntry } from '../models/diary.js';
+import express from "express";
+import { getDiary, getDiaryById, newDiaryEntry } from "../models/diary.js";
 import {
   getAllergies,
   getAllergiesById,
   makeAllergy,
-} from '../models/allergy.js';
-import { getSignUps, newSignUp, linkSignUp } from '../models/signUp.js';
-import { getPatients, getPatientsByDoctor } from '../models/patient.js';
-import { getPrescriptionsById } from '../models/prescription.js';
+} from "../models/allergy.js";
+import { getSignUps, newSignUp, linkSignUp } from "../models/signUp.js";
+import { getPatients, getPatientsByDoctor } from "../models/patient.js";
+import { getPrescriptionsById } from "../models/prescription.js";
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
-  res.send('Home page not in use');
+router.get("/", function (req, res, next) {
+  res.send("Home page not in use");
 });
 
-router.get('/diary', async function (req, res, next) {
+router.get("/diary", async function (req, res, next) {
   const response = await getDiary();
   res.json({ success: true, data: response });
 });
-
 
 //get all diary entries for a specific patient
 router.get("/diary/:id", async function (req, res, next) {
@@ -31,32 +30,39 @@ router.get("/diary/:id", async function (req, res, next) {
 router.post("/diary/:id", async function (req, res, next) {
   const response = await newDiaryEntry(Number(req.params.id), req.body);
   return res.json({ success: true, data: response });
+});
 
-router.get('/allergy/:id', async function (req, res, next) {
+router.get("/allergy/:id", async function (req, res, next) {
   if (req.params.id) {
-    console.log('id given for allergies');
+    console.log("id given for allergies");
     const response = await getAllergiesById(Number(req.params.id));
     return res.json({ success: true, data: response });
   }
   const response = await getAllergies();
   res.json({ success: true, data: response });
 });
-router.get('/signup', async function (req, res, next) {
+
+//add allergy for patient
+router.post("/allergy/:id", async function (req, res, next) {
+  const response = await makeAllergy(Number(req.params.id), req.body);
+  return res.json({ success: true, data: response });
+});
+
+router.get("/signup", async function (req, res, next) {
   const response = await getSignUps();
   res.json({ success: true, data: response });
 });
-router.get('/patients', async function (req, res, next) {
+router.get("/patients", async function (req, res, next) {
   if (req.query.doctoremail !== undefined) {
-    console.log('doc email provided');
+    console.log("doc email provided");
     const response = await getPatientsByDoctor(req.query.doctoremail);
     return res.json({ success: true, data: response });
   }
   const response = await getPatients();
   res.json({ success: true, data: response });
 });
-router.get('/prescriptions/:id', async function (req, res, next) {
+router.get("/prescriptions/:id", async function (req, res, next) {
   const response = await getPrescriptionsById(Number(req.params.id));
   res.json({ success: true, data: response });
-
 });
 export default router;
