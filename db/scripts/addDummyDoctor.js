19964d66837a1bca0975e197b45253573a88e869
brycen <brycensoc@gmail.com>
2022-08-04 10:39:18 +0100

    populate doctor done, need patient ids

--- db/scripts/addDummyDoctor.js
+++ db/scripts/addDummyDoctor.js
@@ -1,1 +1,17 @@
 import pool from '../index.js';
+let doctor = {
+  patients: [],
+  name: 'Ben Snuggles',
+  email: 'bens@gmail.com',
+  gpSurgey: 'Happy Health',
+};
+
+async function populateDoctor(doctor) {
+  const res = await pool.query(
+    'insert into doctor (patients,name,email,gpSurgery) values ($1,$2,$3,$4) returning *',
+    [doctor.patients, doctor.name, doctor.email, doctor.gpSurgey]
+  );
+  console.log(res.rows);
+}
+
+populateDoctor();
