4b14b3084459c52db47628dc16f509277b97ade6
BPGreenwood <53783989+B-P-Greenwood@users.noreply.github.com>
2022-08-04 15:42:25 +0100

    Added Diary routes for the backend

--- routes/users.js
+++ routes/users.js
@@ -18,4 +18,15 @@ router.get("/diary", async function (req, res, next) {
   res.json({ success: true, data: response });
 });
 
+//get all diary entries for a specific patient
+router.get("/diary/:id", async function (req, res, next) {
+  const response = await getDiaryById(Number(req.params.id));
+  return res.json({ success: true, data: response });
+});
+
+// add diary entry for patient
+router.post("/diary/:id", async function (req, res, next) {
+  const response = await newDiaryEntry(Number(req.params.id), req.body);
+  return res.json({ success: true, data: response });
+});
 export default router;
