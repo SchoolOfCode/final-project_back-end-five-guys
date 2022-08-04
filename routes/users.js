18b0fe2fbb465b2e1ca7b8e567030a497eb1f01a
brycen <brycensoc@gmail.com>
2022-08-04 09:50:43 +0100

    server works on 3001

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,13 @@
-import express from "express";
+import express from 'express';
 const router = express.Router();
 
 /* GET users listing. */
-router.get("/", function (req, res, next) {
-  res.send("Home page not in use");
+router.get('/', function (req, res, next) {
+  res.send('Home page not in use');
 });
 
-router.get("/diary", async function (req, res, next) {
-  res.send("Home page not in use");
+router.get('/diary', async function (req, res, next) {
+  res.send('Home page not in use');
 });
 
 export default router;
