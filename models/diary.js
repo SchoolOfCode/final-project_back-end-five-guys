d8b7d5720a84d5b2cd325029ca4cb95937ffa886
BPGreenwood <53783989+B-P-Greenwood@users.noreply.github.com>
2022-08-05 10:38:13 +0100

    Updated addtodiary route to use the patient email

--- models/diary.js
+++ models/diary.js
@@ -1,26 +1,33 @@
-import pool from '../db/index.js';
+import pool from "../db/index.js";
 
 export async function getDiary() {
-  const res = await pool.query('select * from diary');
+  const res = await pool.query("select * from diary");
   return res.rows;
 }
 
 export async function getDiaryById(id) {
-  const res = await pool.query('select * from diary where patient_id=$1', [id]);
+  const res = await pool.query("select * from diary where patient_id=$1", [id]);
   return res.rows;
 }
 export async function getDiaryByEmail(email) {
   const res = await pool.query(
-    'select * from diary join patient on diary.patient_id=patient.patient_id where email=$1',
+    "select * from diary join patient on diary.patient_id=patient.patient_id where email=$1",
     [email]
   );
   return res.rows;
 }
 
-export async function newDiaryEntry(id, entry) {
+export async function newDiaryEntry(email, entry) {
+  const response = await pool.query(
+    "select patient_id from patient where email =$1;",
+    [email]
+  );
+  const id = response.rows[0].patient_id;
+
   const res = await pool.query(
-    'insert into diary (patient_id,date,mood,details) values ($1,$2,$3,$4) returning *;',
+    "insert into diary (patient_id,date,mood,details) values ($1,$2,$3,$4) returning *;",
     [id, entry.date, entry.mood, entry.details]
   );
+
   return res.rows;
 }
--- routes/users.js
+++ routes/users.js
@@ -1,22 +1,27 @@
 import express from "express";
-import { getDiary, getDiaryById, newDiaryEntry } from "../models/diary.js";
+import {
+  getDiary,
+  getDiaryById,
+  newDiaryEntry,
+  getDiaryByEmail,
+} from "../models/diary.js";
 import {
   getAllergies,
   getAllergiesById,
   makeAllergy,
 } from "../models/allergy.js";
-import { getSignUps, newSignUp, linkSignUp } from '../models/signUp.js';
+import { getSignUps, newSignUp, linkSignUp } from "../models/signUp.js";
 import {
   createPatient,
   getPatients,
   getPatientsByDoctor,
-} from '../models/patient.js';
-import { getPrescriptionsById } from '../models/prescription.js';
+} from "../models/patient.js";
+import { getPrescriptionsById } from "../models/prescription.js";
 import {
   getDoctorByEmail,
   getDoctorById,
   addPatientToDoctorList,
-} from '../models/doctor.js';
+} from "../models/doctor.js";
 const router = express.Router();
 
 /* GET users listing. */
@@ -30,19 +35,18 @@ router.get("/diary", async function (req, res, next) {
 });
 
 //get all diary entries for a specific patient
-router.get('/diary/:id', async function (req, res, next) {
-  const response = await getDiaryById(Number(req.params.id));
+router.get("/diary/:email", async function (req, res, next) {
+  const response = await getDiaryByEmail(req.params.email);
   return res.json({ success: true, data: response });
 });
 
 // add diary entry for patient
-router.post('/diary/:id', async function (req, res, next) {
-  const response = await newDiaryEntry(Number(req.params.id), req.body);
+router.post("/diary/:email", async function (req, res, next) {
+  const response = await newDiaryEntry(req.params.email, req.body);
   return res.json({ success: true, data: response });
 });
 
 router.get("/allergy/:id", async function (req, res, next) {
-
   if (req.params.id) {
     console.log("id given for allergies");
     const response = await getAllergiesById(Number(req.params.id));
@@ -72,9 +76,9 @@ router.get("/patients", async function (req, res, next) {
   res.json({ success: true, data: response });
 });
 
-router.post('/patients', async function (req, res, next) {
+router.post("/patients", async function (req, res, next) {
   if (req.query.doctoremail) {
-    console.log('email given for post for new patient');
+    console.log("email given for post for new patient");
     const newPatient = await createPatient(req.body);
     console.log(newPatient);
     const doctorID = await getDoctorByEmail(req.query.doctoremail);
@@ -87,7 +91,7 @@ router.post('/patients', async function (req, res, next) {
   }
   res.json({ success: false, data: {} });
 });
-router.get('/prescriptions/:id', async function (req, res, next) {
+router.get("/prescriptions/:id", async function (req, res, next) {
   const response = await getPrescriptionsById(Number(req.params.id));
   res.json({ success: true, data: response });
 });
