2fe60cc26f068a7be851c90363b89725c40d6079
brycen <brycensoc@gmail.com>
2022-08-08 14:40:44 +0100

    moved semi colon

--- models/pending.js
+++ models/pending.js
@@ -2,6 +2,6 @@ import pool from '../db/index.js';
 
 //gets all pending prescriptions
 export async function getPending() {
-  const res = await pool.query('SELECT * FROM pending;');
+  const res = await pool.query('SELECT * FROM pending');
   return res.rows;
 }
